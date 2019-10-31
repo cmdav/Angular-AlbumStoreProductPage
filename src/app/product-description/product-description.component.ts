77157f043d381653d21d40186d8417f9a6b07ce0
cmdav <davincin222@gmail.com>
2019-10-31 18:54:48 +0100

    service to get Album data_2

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,11 +8,12 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albuminfo;
-  constructor(private  _productService) { }
+  albumInfo;
+
+  constructor( private  _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe( response => this.albuminfo = response );
+    this._productService.getAlbum(1).subscribe( response => this.albumInfo = response );
   }
 
 }
